8affc2e790cbf5fbc047327de3a9a0a727f3f16d
Nick Ethier <ncethier@gmail.com>
2012-09-19 10:16:42 -0600

    initial

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem 'jls-grok'
+gem 'sinatra'
+gem 'json'
+gem 'newrelic_rpm'
---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'grok-pure'
+require 'json'
+require 'newrelic_rpm'
+
+class Application < Sinatra::Base
+  
+  def grok
+    if @grok.nil?
+      @grok = Grok.new
+
+      Dir.foreach('./patterns/') do |item|
+        next if item == '.' or item == '..'
+        @grok.add_patterns_from_file("./patterns/#{item}")
+      end
+    end
+    @grok
+  end
+  post '/grok' do
+    input = params[:input]
+    pattern = params[:pattern]
+
+    begin
+      grok.compile(params[:pattern])
+    rescue 
+      return "Compile ERROR"
+    end
+
+    match = grok.match(params[:input])
+
+    if match
+    #pp match.captures
+      return JSON.pretty_generate(match.captures)
+    end
+
+    return "No Matches"
+  end
+
+  post '/discover' do
+    grok.discover(params[:input])
+  end
+  
+  get '/' do
+    erb :'index'
+  end
+  
+  get '/discover' do
+    erb :'discover'
+  end
+end
